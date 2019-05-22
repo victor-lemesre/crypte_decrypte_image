8d1770cd087dec31624f05bd78bddb03fd2fb192
victor <victor.lemesre@gmail.com>
2019-05-22 23:45:35 +0200

    modification de la fonction pour ne pas avoir d'effets de bord

--- TKcryptage_decryptage.py
+++ TKcryptage_decryptage.py
@@ -1,17 +1,15 @@
 from PIL import Image
 import tkinter as tk
 
-monImage = Image.open("lori.png")
-L, H = monImage.size
 
-
-def image_melange():
+def image_melange(monimage):
+    L, H = monimage.size
     img_horizontale = Image.new("RGB", (L, H))
     img_verticale = Image.new("RGB", (L, H))
 
     for x in range(0, L, 1):
         for y in range(0, H, 1):
-            pixel = monImage.getpixel((x, y))
+            pixel = monimage.getpixel((x, y))
             if y % 2 == 0:
                 img_verticale.putpixel((L - 1 - x, y), (pixel[0], pixel[1], pixel[2]))
             else:
@@ -25,20 +23,19 @@ def image_melange():
             else:
                 img_horizontale.putpixel((x, y), (pixel[0], pixel[1], pixel[2]))
 
-    img_horizontale.save("ImageM.png")
-    img_horizontale.show()
+    return img_horizontale
 
 
-def image_ranger():
+def image_ranger(myimage):
     # Attention cette variable n'est pas la même que celles déclarée au tout debut
     # il faut lui donner un autre nom pour eviter la confusion
-    monImage = Image.open("ImageMelange.png")
+
     img_horizontale = Image.new("RGB", (L, H))
     img_verticale = Image.new("RGB", (L, H))
 
     for y in range(0, H, 1):
         for x in range(0, L, 1):
-            pixel = monImage.getpixel((x, y))
+            pixel = myimage.getpixel((x, y))
             if x % 2 == 0:
                 img_horizontale.putpixel((x, H - 1 - y), (pixel[0], pixel[1], pixel[2]))
             else:
@@ -56,15 +53,22 @@ def image_ranger():
     img_verticale.show()
 
 
+def affichage():
+    can1.itemconfig(photo,image=img_horizontale)
+
+
+
+monimage = Image.open("lori.png")
+L, H = monimage.size
 fen1 = tk.Tk()
 can1 = tk.Canvas(fen1, width=L, height=H, bg='white')
 can1.pack()
 photo = tk.PhotoImage(file='lori.png')
-img = can1.create_image(L / 2, H / 2, image=photo)
-bouton1 = tk.Button(fen1, text='mélanger image', command=image_melange)
+can1.create_image(L / 2, H / 2, image=photo)
+bouton1 = tk.Button(fen1, text='mélanger image', command=lambda: image_melange(monimage))
 bouton1.pack()
 
-bouton2 = tk.Button(fen1, text='ranger image', command=image_ranger)
+bouton2 = tk.Button(fen1, text='ranger image', command=lambda: image_ranger(myimage))
 bouton2.pack()
 
 fen1.mainloop()
