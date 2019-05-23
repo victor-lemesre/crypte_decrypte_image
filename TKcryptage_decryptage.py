da9d8a60fd6e98dfbb4462f23ed391c9864a233f
victor <victor.lemesre@gmail.com>
2019-05-23 17:53:28 +0200

    construction de la fonction affichage mais problème avec ce que return la fonction melange

--- TKcryptage_decryptage.py
+++ TKcryptage_decryptage.py
@@ -2,14 +2,14 @@ from PIL import Image
 import tkinter as tk
 
 
-def image_melange(monimage):
-    L, H = monimage.size
+def image_melange(image):
+    L, H = image.size
     img_horizontale = Image.new("RGB", (L, H))
     img_verticale = Image.new("RGB", (L, H))
 
     for x in range(0, L, 1):
         for y in range(0, H, 1):
-            pixel = monimage.getpixel((x, y))
+            pixel = image.getpixel((x, y))
             if y % 2 == 0:
                 img_verticale.putpixel((L - 1 - x, y), (pixel[0], pixel[1], pixel[2]))
             else:
@@ -26,7 +26,7 @@ def image_melange(monimage):
     return img_horizontale
 
 
-def image_ranger(monimage):
+def image_ranger(image):
     # Attention cette variable n'est pas la même que celles déclarée au tout debut
     # il faut lui donner un autre nom pour eviter la confusion
 
@@ -35,7 +35,7 @@ def image_ranger(monimage):
 
     for y in range(0, H, 1):
         for x in range(0, L, 1):
-            pixel = myimage.getpixel((x, y))
+            pixel = image.getpixel((x, y))
             if x % 2 == 0:
                 img_horizontale.putpixel((x, H - 1 - y), (pixel[0], pixel[1], pixel[2]))
             else:
@@ -49,14 +49,19 @@ def image_ranger(monimage):
             else:
                 img_verticale.putpixel((x, y), (pixel[0], pixel[1], pixel[2]))
 
-    img_verticale.save("ImageM.png")
-    img_verticale.show()
+    return img_verticale
 
 
-def affichage(canvas):
-    canvas.find_all()
-    canvas.itemconfig(photo,image=img_horizontale)
+def affichage_melange(canvas, image):
+    image_melange(image)
+    photo = canvas.find_all()
+    canvas.itemconfig(photo, image=img_horizontale)
 
+
+def affichage_ranger(canvas, image):
+    image_ranger(image)
+    photo = canvas.find_all()
+    canvas.itemconfig(photo, image=img_verticale)
 
 
 monimage = Image.open("lori.png")
@@ -64,12 +69,12 @@ L, H = monimage.size
 fen1 = tk.Tk()
 can1 = tk.Canvas(fen1, width=L, height=H, bg='white')
 can1.pack()
-photo = tk.PhotoImage(file='lori.png')
-can1.create_image(L / 2, H / 2, image=photo)
-bouton1 = tk.Button(fen1, text='mélanger image', command=lambda: affichage(can1))
+photo1 = tk.PhotoImage(file='lori.png')
+can1.create_image(L / 2, H / 2, image=photo1)
+bouton1 = tk.Button(fen1, text='mélanger image', command=lambda: affichage_melange(can1, monimage))
 bouton1.pack()
 
-bouton2 = tk.Button(fen1, text='ranger image', command=lambda: image_ranger(monimage))
+bouton2 = tk.Button(fen1, text='ranger image', command=lambda: affichage_ranger(can1, monimage))
 bouton2.pack()
 
 fen1.mainloop()
